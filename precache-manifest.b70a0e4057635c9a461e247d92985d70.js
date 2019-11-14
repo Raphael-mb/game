abe0443b50f924266c6c598bdf4f24260b6bec84
Raphael Bradbury <raphybradbury@gmail.com>
2019-11-14 02:04:52 +0200

    Updates

---
+++ precache-manifest.b70a0e4057635c9a461e247d92985d70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90caaa60d3993cffcfbdd5d8c44bc6fa",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "874e6f5a3e197a3636ce",
+    "url": "/game/static/css/main.ef8640e2.chunk.css"
+  },
+  {
+    "revision": "66b8b03938ecabc49265",
+    "url": "/game/static/js/2.66224a4f.chunk.js"
+  },
+  {
+    "revision": "874e6f5a3e197a3636ce",
+    "url": "/game/static/js/main.8db85d2d.chunk.js"
+  },
+  {
+    "revision": "333071aac412a4ae4928",
+    "url": "/game/static/js/runtime-main.0802fdb3.js"
+  }
+]);
